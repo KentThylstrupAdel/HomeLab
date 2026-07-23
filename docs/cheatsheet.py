57bc1ecdb593426888c9a632a1da3ea010d098f6
Kent Thylstrup Adel <75899790+KentThylstrupAdel@users.noreply.github.com>
2026-07-23 22:03:24 +0200

    Fix formatting in Ansible section of cheatsheet

--- docs/cheatsheet.py
+++ docs/cheatsheet.py
@@ -1,9 +1,8 @@
 # Cheatsheet
 
 ## Ansible
-Enter Ansible through Distrobox using 
-
+#Enter Ansible through Distrobox using 
 distrobox enter ansible-box
 
-Running a playbook:
+#Running a playbook:
 ansible-playbook -i inventory.ini playbooks/update.yml -K
