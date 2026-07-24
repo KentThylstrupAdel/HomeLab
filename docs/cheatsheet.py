9c5de89713577687f90abf6f11a31343b58a6a27
Kent Thylstrup Adel <75899790+KentThylstrupAdel@users.noreply.github.com>
2026-07-24 12:49:27 +0200

    Update cheatsheet.py

--- docs/cheatsheet.py
+++ docs/cheatsheet.py
@@ -6,3 +6,24 @@ distrobox enter ansible-box
 
 #Running a playbook:
 ansible-playbook -i inventory.ini playbooks/update.yml -K
+
+#Git stuff
+#get my github stuff
+git clone <my github>
+
+#update my github stuff
+git pull
+
+#check what changes made locally
+git status
+
+#add to staging area
+git add .
+#or
+git add playbook/update.yml
+
+#Commit
+git commit -m "message is needed"
+
+#Push
+git push
