015af54c48d25e17906d45172718cf9aa3a552cc
KentThylstrupAdel <kentadel@gmail.com>
2026-09-03 16:48:37 +0200

    Update ansible Privilege escaltion and Kubernetes status script

--- scripts/k8s_status.py
+++ scripts/k8s_status.py
@@ -1,12 +1,49 @@
 import subprocess
 import json
+import sys
 
-result = subprocess.run(
-    ["kubectl", "get", "nodes", "-o", "json"],
-    capture_output=True,
-    text=True
-)
 
-nodes = json.loads(result.stdout)
+def get_nodes():
+    result = subprocess.run(
+        ["kubectl", "get", "nodes", "-o", "json"],
+        capture_output=True,
+        text=True
 
-print(nodes["items"][0]["metadata"]["name"])
+    )
+
+    if result.returncode != 0:
+        print("ERROR: Unable to communicate with Kubernetes cluster.")
+        print(result.stderr)
+        sys.exit(1)
+    try:
+        nodes = json.loads(result.stdout)
+    except json.JSONDecodeError:
+        print("ERROR: Unable to parse Kubernetes nodes output.")
+        sys.exit(1)
+
+    return nodes
+
+
+def get_node_ready_status(node):
+    for condition in node["status"]["conditions"]:
+        if condition["type"] == "Ready":
+            return condition["status"] == "True"
+    return False
+
+nodes = get_nodes()
+
+all_nodes_ready = True
+
+for node in nodes["items"]:
+    print(f"Node Name: {node['metadata']['name']}")
+    ready_status = get_node_ready_status(node)
+    if ready_status:
+        print("  status: READY")
+    else:
+        print("  status: NOT READY")
+        all_nodes_ready = False
+
+if all_nodes_ready:
+    print("All nodes are ready.")
+else:
+    print("Some nodes are not ready.")
